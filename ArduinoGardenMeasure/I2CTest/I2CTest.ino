4e039d6b57a81b5fb2928d5d7b641ee39be4583c
Kujak <Kujak@HELLBOX>
2016-06-10 01:20:49 +0200

    I2CTest

--- ArduinoGardenMeasure/I2CTest/I2CTest.ino
+++ ArduinoGardenMeasure/I2CTest/I2CTest.ino
@@ -1,10 +1,9 @@
 /*
- Name:		Timer.ino
- Created:	6/9/2016 11:36:00 PM
+ Name:		I2CTest.ino
+ Created:	6/10/2016 1:13:03 AM
  Author:	Kujak
 */
 
-#include <TimerOne.h>
 #include <Wire.h>
 
 #define sensor01 0x20
@@ -20,8 +19,6 @@ void setup(void)
 {
 	Wire.begin();
 	I2CwriteRegister8bit(sensor01, 6); //reset	
-	Timer1.initialize(1000000);
-	Timer1.attachInterrupt(getI2C);
 	Serial.begin(9600);
 	Serial.println("setup");
 }
@@ -29,23 +26,22 @@ void setup(void)
 void loop(void)
 {
 	Serial.println("loop");
-	noInterrupts();
 	I2Cmoist = moist;
 	I2Ctemp = temp;
 	I2Clight = light;
-	interrupts();
 
-	Serial.print("blinkCount = ");
-	Serial.println(moist);
-	delay(100);
+	Serial.print("moist = ");
+	Serial.println(I2Cmoist);
+	delay(1000);
+	getI2C();
 }
 void getI2C(void)
 {
 	Serial.println("getI2C");
-//	int moist = I2CreadRegister16bit(sensor01, 0);
-//	int temp = I2CreadRegister16bit(sensor01, 5);
-//	I2CwriteRegister8bit(sensor01, 3);
-//	int light = I2CreadRegister16bit(sensor01, 4);
+	int moist = I2CreadRegister16bit(sensor01, 0);
+	//	int temp = I2CreadRegister16bit(sensor01, 5);
+	//	I2CwriteRegister8bit(sensor01, 3);
+	//	int light = I2CreadRegister16bit(sensor01, 4);
 }
 
 void I2CwriteRegister8bit(int addr, int value)
