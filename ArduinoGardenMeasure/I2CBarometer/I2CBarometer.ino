611b19b060fa7107be7c1c1f95de00d7fa23293f
Kujak <Kujak@HELLBOX>
2016-06-17 16:16:10 +0200

    Barometer und Licht Test

--- ArduinoGardenMeasure/I2CBarometer/I2CBarometer.ino
+++ ArduinoGardenMeasure/I2CBarometer/I2CBarometer.ino
@@ -9,6 +9,11 @@
 
 Adafruit_MPL115A2 mpl115a2;
 
+int sensorPin = A0;    // select the input pin for the potentiometer
+
+float rawRange = 1024; // 3.3v
+float logRange = 5.0; // 3.3v = 10^5 lux
+
 void setup(void)
 {
 	Serial.begin(9600);
@@ -31,6 +36,19 @@ void loop(void)
 
 	temperatureC = mpl115a2.getTemperature();
 	Serial.print("Temp (*C): "); Serial.print(temperatureC, 1); Serial.println(" *C");
+	
+	int rawValue = analogRead(sensorPin);
+
+	Serial.print("Raw = ");
+	Serial.print(rawValue);
+	Serial.print(" - Lux = ");
+	Serial.println(RawToLux(rawValue));
 
 	delay(1000);
+}
+
+float RawToLux(int raw)
+{
+	float logLux = raw * logRange / rawRange;
+	return pow(10, logLux);
 }
